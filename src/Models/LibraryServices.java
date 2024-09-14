bcaa34f6a5d1de2063bdd31d6c77c7b98e4b5e55
hudaamr <94346307+hudaamr@users.noreply.github.com>
2024-09-14 08:18:05 +0300

    Update LibraryServices.java

--- src/Models/LibraryServices.java
+++ src/Models/LibraryServices.java
@@ -2,7 +2,7 @@ package Models;
 
 import Interfaces.IBookRepository;
 import Interfaces.IMemberRepository;
-
+import Services.BorrowService;
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -79,42 +79,96 @@ public class LibraryServices {
 
     public static void updateBook() {
         bookRepository.updateBook(inputString("ISBN"));
-    }
+        Book book = bookRepository.findBookByIsbn(isbn);
+  if (book != null) {
+            System.out.println("Current details of the book: " + book);
+            System.out.println("Enter new details for the book:");
+            String newTitle = inputString("Title");
+            String newAuthor = inputString("Author");
+            LocalDate newPublicationDate = inputDate();
 
+            book.setTitle(newTitle);
+            book.setAuthor(newAuthor);
+            book.setPublicationDate(newPublicationDate);
+
+            bookRepository.updateBook(book);
+            System.out.println("Book updated successfully.");
+        } else {
+            System.out.println("Book not found with ISBN: " + isbn);
+        }
+    }
     public static void updateMember() {
         memberRepository.updateMember(inputMember());
+        String email = inputString("Email");
+        Member member = memberRepository.findMemberByEmail(email);
+
+        if (member != null) {
+            System.out.println("Current details of the member: " + member);
+            System.out.println("Enter new details for the member:");
+            String newName = inputString("Name");
+            String newPhone = inputString("Phone");
+
+            member.setName(newName);
+            member.setPhone(newPhone);
+
+            memberRepository.updateMember(member);
+            System.out.println("Member updated successfully.");
+        } else {
+            System.out.println("Member not found with email: " + email);
+        }
     }
 
-    public static void getBooks() {
+  public static void getBooks() {
         for (Book book : bookRepository.getBooks()) {
             System.out.println(book);
         }
     }
 
-    private static void showOptions() {
+    // Borrow a Book
+    public static void borrowBook() {
+        String isbn = inputString("ISBN");
+        String memberId = inputString("Member ID");
+        LocalDate dueDate = inputDate();  // Prompt the user for the due date
+
+        BorrowService borrowService = new BorrowService(bookRepository, memberRepository);
+        borrowService.borrowBook(isbn, memberId, dueDate);  // Call borrowBook method from BorrowService
+    }
+
+    // Return a Book
+    public static void returnBook() {
+        String isbn = inputString("ISBN");
+        String memberId = inputString("Member ID");
+        LocalDate returnDate = LocalDate.now();  // Assume the return date is the current date
+
+        BorrowService borrowService = new BorrowService(bookRepository, memberRepository);
+        borrowService.returnBook(isbn, memberId, returnDate);  // Call returnBook method from BorrowService
+    }
+  private static void showOptions() {
         System.out.println("1. Add Book");
         System.out.println("2. Register Member");
         System.out.println("3. Update Book");
         System.out.println("4. Update Member");
         System.out.println("5. Get Books");
+        System.out.println("6. Borrow Book");
+        System.out.println("7. Return Book");
         System.out.println("0. Exit");
     }
 
-
-
-    private static void  processOption(int option) {
+    // Process user input
+    private static void processOption(int option) {
         switch (option) {
             case 1 -> addBook();
             case 2 -> registerMember();
             case 3 -> updateBook();
             case 4 -> updateMember();
             case 5 -> getBooks();
+            case 6 -> borrowBook();  // Borrow book option
+            case 7 -> returnBook();  // Return book option
             case 0 -> {
                 System.out.println("Exiting...");
                 System.exit(0);
             }
             default -> System.out.println("Invalid choice, please choose again.");
-
         }
     }
 
